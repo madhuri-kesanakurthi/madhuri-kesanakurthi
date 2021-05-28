a9596999ff438244d3cf13ac1fe42a67103a5198
Madhuri-kesanakurthi <84342899+madhuri-kesanakurthi@users.noreply.github.com>
2021-05-28 12:42:39 +0530

    Create while.py

---
+++ while.py
@@ -0,0 +1,24 @@
+i=int(input('enter starting value'))
+n=int(input('enter ending value'))
+inc=int(input('enter incremented value'))
+
+while i<=n:
+   print (i)
+   i=i+inc
+
+enter starting value2
+enter ending value10
+enter incremented value2
+2
+4
+6
+8
+10
+>>> 
+==== RESTART: C:/Users/ADMIN/AppData/Local/Programs/Python/Python39/loops.py ===
+enter starting value3
+enter ending value8
+enter incremented value2
+3
+5
+7
