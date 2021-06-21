198324ec551c42db3245539dc4786a9dbb713076
Madhuri-kesanakurthi <84342899+madhuri-kesanakurthi@users.noreply.github.com>
2021-06-21 18:11:17 +0530

    Create Find(),index().py

---
+++ Find(),index().py
@@ -0,0 +1,24 @@
+s=input("enter string ") 
+print(s) 
+res=s.find("c")
+ print(res)
+
+ enter string python programming
+ python programming 
+-1 
+
+s=input("enter string ")
+print(s)
+ res=s.index("c")
+ print(res) 
+
+enter string Python programming
+ Python programming ---------------------------------------------------------------------------
+ ValueError Traceback (most recent call last) 
+<ipython-input-13-533d23b94eef> in <module>()
+ 1 s=input("enter string ")
+ 2 print(s)
+ ----> 3 res=s.index("c")
+ 4 print(res)
+
+ ValueError: substring not found
