f90dca21e08b23d800eb5b7d63719aa02238359a
Madhuri-kesanakurthi <84342899+madhuri-kesanakurthi@users.noreply.github.com>
2021-06-25 11:39:14 +0530

    Create Lab4.1.py

---
+++ Lab4.1.py
@@ -0,0 +1,12 @@
+s=input('enter a string')
+print('vowels in the string are')
+for char in  s:
+    if char=='a' or char=='e' or char=='i' or char=='o' or char=='u':
+        print(char)
+'''
+Output:
+Enter a string:Madhuri
+a
+i
+u
+'''
