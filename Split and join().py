effbfe8b6972ca2eee1a549d407b31589bc552cc
Madhuri-kesanakurthi <84342899+madhuri-kesanakurthi@users.noreply.github.com>
2021-07-02 01:38:01 +0530

    Create Split and join().py

---
+++ Split and join().py
@@ -0,0 +1,14 @@
+
+# String Split and Join in Python - HackerRank Solution
+def split_and_join(line):
+    # write your code here
+    # String Split and Join in Python - HackerRank Solution START
+    Output = line.split();
+    Output = "-".join(Output)
+    return Output;
+    # String Split and Join in Python - HackerRank Solution END
+    
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
